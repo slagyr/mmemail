91c9b74f9c084804d6175e68205480f5bee17a6a
Simon Jagoe <azriphale@gmail.com>
2010-10-26 15:52:12 +0200

    Added STARTTLS support for non-ssl ports (e.g. STARTTLS on port 25)
    mmemail was unable to send email with the following configuration:
    
    {:host "smtp.example.com"
     :port 25
     :ssl true
     :user "azriphale@example.com"
     :password "password"}
    
    It was always forcing an SSLSocketFactory to be used, even in the case
    where we needed to connect to a plaintext socket and initiate a TLS
    connection using STARTTLS.
    
    This patch allows STARTTLS to be used on a plaintext socket to
    initiate encryption after the connection has been established. To use
    this, the above configuration would change to:
    
    {:host "smtp.example.com"
     :port 25
     :starttls true
     :user "azriphale@example.com"
     :password "password"}

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mmemail "1.0.0"
+(defproject mmemail "1.0.1"
   :description "Simple email delivery with just one jar!"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]])
